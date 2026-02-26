f994178adca027809a82ec7cf5ef4751d746b8a3
Guntur Padilah <kangwe563@gmail.com>
2026-02-26 23:38:37 +0700

    Update dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -4,3 +4,5 @@ config();
 import '@/ai/flows/chat-with-litera-ai.ts';
 import '@/ai/flows/summarize-book-content.ts';
 import '@/ai/flows/screenplay-helper-flow.ts';
+import '@/ai/flows/novel-helper-flow.ts';
+import '@/ai/flows/poetry-helper-flow.ts';
